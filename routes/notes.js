9b2eaa4c292825edb817cbfcf1c9b8ffc65c887c
Reiq.Laptop <reiqvideos@gmail.com>
2021-08-21 21:06:33 -0400

    Configuración del middleware method-override para hacer actiones por PUT en el formulario

--- routes/notes.js
+++ routes/notes.js
@@ -5,6 +5,6 @@ const notes_controller = require('../controllers/notes');
 router.get('/note/add', notes_controller.add);
 router.post('/note/add', notes_controller.new_note);
 router.get('/note/edit/:id', notes_controller.edit);
-router.post('/note/edit/:id', notes_controller.update_note);
+router.put('/note/edit/:id', notes_controller.update_note);
 
 module.exports = router;
