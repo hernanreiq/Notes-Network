e00b6e1a32712227f9eaafe9e4e92e816672e88f
Reiq.Laptop <reiqvideos@gmail.com>
2021-08-26 14:27:44 -0400

    Ventana de about maquetada y estilizada

--- controllers/index.js
+++ controllers/index.js
@@ -4,6 +4,15 @@ const index_controller = {
     home: async function (req, res) {
         const notes = await Note.find({visibility: true}).sort({_id: -1});
         res.render('index', {notes});
+    },
+    about: function(req, res){
+        res.render('about');
+    },
+    author: function(req, res){
+        res.redirect('https://bit.ly/hernanreiq');
+    },
+    dev: function(req, res){
+        res.redirect('https://github.com/hernanreiq/Notes-Network');
     }
 };
 
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const index_controller = require('../controllers/index');
 
 router.get('/', index_controller.home);
+router.get('/about', index_controller.about);
+router.get('/author', index_controller.author);
+router.get('/dev', index_controller.dev);
+
 
 module.exports = router;
